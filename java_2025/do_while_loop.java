e3256996eed1e8401a8a0ebc47b69b14eb459b0e
J._.N._.TAPANIYA <jenish.tapaniya123@gmail.com>
2025-12-27 14:48:02 +0530

    Add files via upload

---
+++ java_2025/do_while_loop.java
@@ -0,0 +1,13 @@
+class do_while_loop
+{
+	public static void main(String[] args)
+	{
+		int i=10;
+		do
+		{
+			System.out.println(i);
+			i--;
+		}
+		while(i>=0);
+	}
+}
---
+++ java_2025/while_loop.java
@@ -0,0 +1,12 @@
+class while_loop
+{
+	public static void main(String[] args)
+	{
+		int i=10;
+		while(i>=0)
+		{
+			System.out.println(i);
+			i--;
+		}
+	}
+}
