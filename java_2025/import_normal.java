39339a609b6113a8e9f810be4ef56806c517bc95
J._.N._.TAPANIYA <jenish.tapaniya123@gmail.com>
2026-02-04 19:05:03 -0800

    Add files via upload

---
+++ java_2025/import_normal.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class import_normal
+{
+	public static void main(String[] jenish)
+	{
+		System.out.println(Math.sqrt(4));
+		System.out.println(Math.pow(2,4));
+		System.out.println(Math.abs(-6.5));
+	}
+}
---
+++ java_2025/import_static.java
@@ -0,0 +1,10 @@
+import static java.lang.Math.*;
+class import_static
+{
+	public static void main(String[] jenish)
+	{
+		System.out.println(sqrt(4));
+		System.out.println(pow(2,4));
+		System.out.println(abs(-6.5));
+	}
+}
